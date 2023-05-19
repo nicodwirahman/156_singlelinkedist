877c3a86e65053193abb9710860160260860a408
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-19 13:21:06 +0700

    13

--- 156_singlelinkedist/156_singlelinkedist.cpp
+++ 156_singlelinkedist/156_singlelinkedist.cpp
@@ -97,4 +97,10 @@ void traverse() {
 		system("cls");
 		return;
 	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL)
+			cout << "NIM" << currentNode->noMhs << ", Nama:" << currentNode->name << endl;
+		currentNode = currentNode->next;
+	}
 }
